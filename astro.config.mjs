8535d1c8b55d4e83794fd5c16883cae382ad5422
AdezAtl <adezatl4336@gmail.com>
2026-03-22 23:21:38 +0100

    removed node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
