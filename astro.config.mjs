8d64074c99ddaec52c3189c875682cc1929217c9
AdezAtl <adezatl4336@gmail.com>
2026-06-28 12:43:35 +0100

    fix vercel adapter import path and restore output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 const isVercel = process.env.VERCEL === "1";
 
 export default defineConfig({
+  output: "server",
   adapter: isVercel ? vercel() : node({ mode: "standalone" }),
   security: {
     checkOrigin: true,
